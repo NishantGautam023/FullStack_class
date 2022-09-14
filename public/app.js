40fd0287210cbd34e42ae4da34894b6e34ab2e63
NishantGautam023 <47427428+NishantGautam023@users.noreply.github.com>
2022-09-13 20:39:00 -0400

    4 files modified, 1 added

--- public/app.js
+++ public/app.js
@@ -1,7 +1,24 @@
+class Hello extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("h2", null, "Hello i am class component ");
+  }
+
+}
+
+const Employee = props => {
+  return /*#__PURE__*/React.createElement("h2", null, "Hello; ", props.firstName, " ", props.lastName);
+};
+
+const firstName = 'James';
+const lastName = 'Bond';
+
 const App = () => {
   return /*#__PURE__*/React.createElement("div", {
     id: "child"
-  }, /*#__PURE__*/React.createElement("h2", null, "Welcome to Adv Full-Stack Programming"));
+  }, /*#__PURE__*/React.createElement("h2", null, "Welcome to Adv Full-Stack Programming"), /*#__PURE__*/React.createElement(Employee, {
+    firstName: firstName,
+    lastName: lastName
+  }), /*#__PURE__*/React.createElement(Hello, null));
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,27 @@
+class Hello extends React.Component {
+  render() {
+    return <h2>Hello i am class component </h2>;
+  }
+}
+
+const Employee = (props) => {
+  return (
+    <h2>
+      Hello; {props.firstName} {props.lastName}
+    </h2>
+  );
+};
+
+const firstName = 'James';
+const lastName = 'Bond';
+
 const App = () => {
   return (
     <div id="child">
       <h2>Welcome to Adv Full-Stack Programming</h2>
+
+      <Employee firstName={firstName} lastName={lastName} />
+      <Hello />
     </div>
   );
 };
