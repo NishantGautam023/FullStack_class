bcfb0ac4cc9bdc9c210be1adbc5579e1e1102e4f
NishantGautam023 <47427428+NishantGautam023@users.noreply.github.com>
2022-09-16 17:55:58 -0400

    Using IssueTable

--- public/app.js
+++ public/app.js
@@ -1,25 +1,39 @@
-class Hello extends React.Component {
-  render() {
-    return /*#__PURE__*/React.createElement("h2", null, "Hello i am class component ");
-  }
-
-}
-
-const Employee = props => {
-  return /*#__PURE__*/React.createElement("h2", null, "Hello; ", props.firstName, " ", props.lastName);
+const IssueTable = () => {
+  const rowStyle = {
+    border: '1px solid black'
+  };
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "IssueTable"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+    style: rowStyle
+  }, "ID"), /*#__PURE__*/React.createElement("th", {
+    style: rowStyle
+  }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "1"), /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "This is First Issue")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "2"), /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "This is Second Issue")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "3"), /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, "This is Third Issue")))));
 };
 
-const firstName = 'James';
-const lastName = 'Bond';
+const IssueFilter = () => {
+  return /*#__PURE__*/React.createElement("h2", null, "IssueFilter");
+};
 
-const App = () => {
+const AddIssue = () => {
+  return /*#__PURE__*/React.createElement("h2", null, "AddIssue");
+};
+
+const IssueList = () => {
   return /*#__PURE__*/React.createElement("div", {
     id: "child"
-  }, /*#__PURE__*/React.createElement("h2", null, "Welcome to Adv Full-Stack Programming"), /*#__PURE__*/React.createElement(Employee, {
-    firstName: firstName,
-    lastName: lastName
-  }), /*#__PURE__*/React.createElement(Hello, null));
+  }, /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(AddIssue, null));
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/React.createElement(App, null));
+root.render( /*#__PURE__*/React.createElement(IssueList, null));
--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,54 @@
-class Hello extends React.Component {
-  render() {
-    return <h2>Hello i am class component </h2>;
-  }
-}
-
-const Employee = (props) => {
+const IssueTable = () => {
+  const rowStyle = { border: '1px solid black' };
   return (
-    <h2>
-      Hello; {props.firstName} {props.lastName}
-    </h2>
+    <div>
+      <h2>IssueTable</h2>
+      <table>
+        <thead>
+          <tr>
+            <th style={rowStyle}>ID</th>
+            <th style={rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={rowStyle}>1</td>
+            <td style={rowStyle}>This is First Issue</td>
+          </tr>
+          <tr>
+            <td style={rowStyle}>2</td>
+            <td style={rowStyle}>This is Second Issue</td>
+          </tr>
+
+          <tr>
+            <td style={rowStyle}>3</td>
+            <td style={rowStyle}>This is Third Issue</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-const firstName = 'James';
-const lastName = 'Bond';
+const IssueFilter = () => {
+  return <h2>IssueFilter</h2>;
+};
 
-const App = () => {
+const AddIssue = () => {
+  return <h2>AddIssue</h2>;
+};
+const IssueList = () => {
   return (
     <div id="child">
-      <h2>Welcome to Adv Full-Stack Programming</h2>
-
-      <Employee firstName={firstName} lastName={lastName} />
-      <Hello />
+      <IssueTable />
+      <hr />
+      <IssueFilter />
+      <hr />
+      <AddIssue />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<IssueList />);
