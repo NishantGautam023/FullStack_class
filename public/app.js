71ba5497e9cab9220b434a3d6c696b5c37275b1a
NishantGautam023 <47427428+NishantGautam023@users.noreply.github.com>
2022-09-23 17:55:09 -0400

    2 files modified

--- public/app.js
+++ public/app.js
@@ -1,25 +1,132 @@
 class Hello extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("h2", null, "Hello i am class component ");
+    return /*#__PURE__*/React.createElement("h2", null, "Hello I am class component");
   }
 
 }
 
-const Employee = props => {
-  return /*#__PURE__*/React.createElement("h2", null, "Hello; ", props.firstName, " ", props.lastName);
-};
-
-const firstName = 'James';
-const lastName = 'Bond';
-
-const App = () => {
+const Greet = () => {
   return /*#__PURE__*/React.createElement("div", {
     id: "child"
-  }, /*#__PURE__*/React.createElement("h2", null, "Welcome to Adv Full-Stack Programming"), /*#__PURE__*/React.createElement(Employee, {
+  }, /*#__PURE__*/React.createElement("h2", null, "Welcome to Adv Full-Stack Programming"), /*#__PURE__*/React.createElement(Hello, null));
+}; // first method
+
+
+const Employee = ({
+  firstName,
+  lastName
+}) => {
+  return /*#__PURE__*/React.createElement("h2", null, "Hello: ", firstName, " ", lastName);
+}; // second method
+
+/* const Employee = (props) =>{
+    return(
+    <h2>Hello: {props.firstName} {props.lastName}</h2>
+    )
+} */
+
+/*
+Third method
+const Employee = ({firstName,lastName}) =>{
+    const {firstName,lastName}=props;
+    return(
+    <h2>Hello: {firstName} {lastName}</h2>
+    )
+
+*/
+
+
+const firstName = "James";
+const lastName = "Bond";
+
+const App = () => {
+  return /*#__PURE__*/React.createElement(Employee, {
     firstName: firstName,
     lastName: lastName
-  }), /*#__PURE__*/React.createElement(Hello, null));
+  });
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( /*#__PURE__*/React.createElement(App, null));
+const IssueFilter = () => {
+  return /*#__PURE__*/React.createElement("h2", null, "This is placeholder for IssueFilter");
+};
+
+const IssueRow = props => {
+  //const rowStyle = { border: "2px solid black" };
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, props.Id), /*#__PURE__*/React.createElement("td", null, props.Status), /*#__PURE__*/React.createElement("td", null, props.Owner), /*#__PURE__*/React.createElement("td", null, props.Effort), /*#__PURE__*/React.createElement("td", null, props.Create.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Due.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Title));
+};
+
+const IssueTable = () => {
+  //Assume you are fetching data from API
+  const tempIssues = [{
+    Id: 1,
+    Status: "Assigned",
+    Owner: "Person-A",
+    Effort: 14,
+    Create: new Date("2022-09-23"),
+    Due: new Date("2022-09-20"),
+    Title: "This is first isuue"
+  }, {
+    Id: 2,
+    Status: "Resolved",
+    Owner: "Person-A",
+    Effort: 10,
+    Create: new Date("2022-09-24"),
+    Due: new Date("2022-09-20"),
+    Title: "This is second isuue"
+  }];
+  const newIssue = {
+    Status: "Assigned",
+    Owner: "Person-C",
+    Effort: 24,
+    Create: new Date("2022-09-12"),
+    Due: new Date("2022-09-19"),
+    Title: "This is third isuue"
+  };
+  const [allIssues, setAllIssues] = React.useState(tempIssues);
+  let counter = 0;
+  React.useEffect(() => {
+    console.log(counter);
+    setTimeout(() => {
+      console.log(counter);
+      counter++;
+      AddSingleIssue(newIssue);
+    }, 3000);
+  }, [allIssues]);
+  /* setTimeout(() => {
+      setAllIssues(tempIssues)
+  }, 2000)
+  */
+
+  const AddSingleIssue = newIssue => {
+    console.log(allIssues);
+    newIssue.Id = allIssues.length + 1;
+    let issueList = allIssues.slice();
+    issueList.push(newIssue);
+    setAllIssues(issueList); //setAllIssues([...tempIssues,...newIssue])
+  };
+
+  const AllIssuesRow = allIssues.map(issue => /*#__PURE__*/React.createElement(IssueRow, {
+    key: issue.Id,
+    Id: issue.Id,
+    Status: issue.Status,
+    Owner: issue.Owner,
+    Effort: issue.Effort,
+    Create: issue.Create,
+    Due: issue.Due,
+    Title: issue.Title
+  }));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "This is placeholder for IssueTable"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Id"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Create"), /*#__PURE__*/React.createElement("th", null, "Due"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, AllIssuesRow)));
+};
+
+const AddIssue = () => {
+  return /*#__PURE__*/React.createElement("h2", null, "This is placeholder for IssueFilter");
+};
+
+const IssueList = () => {
+  return /*#__PURE__*/React.createElement("div", {
+    id: "issueList"
+  }, /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(AddIssue, null));
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( /*#__PURE__*/React.createElement(IssueList, null));
--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,160 @@
 class Hello extends React.Component {
-  render() {
-    return <h2>Hello i am class component </h2>;
-  }
+    render() {
+        return (
+            <h2>Hello I am class component</h2>
+        )
+    }
 }
 
-const Employee = (props) => {
-  return (
-    <h2>
-      Hello; {props.firstName} {props.lastName}
-    </h2>
-  );
-};
+const Greet = () => {
+    return (
+        <div id="child">
+            <h2>Welcome to Adv Full-Stack Programming</h2>
+            <Hello />
+        </div>
+    )
+}
+// first method
+const Employee = ({ firstName, lastName }) => {
+    return (
+        <h2>Hello: {firstName} {lastName}</h2>
+    )
+}
+// second method
+/* const Employee = (props) =>{
+    return(
+    <h2>Hello: {props.firstName} {props.lastName}</h2>
+    )
+} */
+/*
+Third method
+const Employee = ({firstName,lastName}) =>{
+    const {firstName,lastName}=props;
+    return(
+    <h2>Hello: {firstName} {lastName}</h2>
+    )
 
-const firstName = 'James';
-const lastName = 'Bond';
-
+*/
+const firstName = "James";
+const lastName = "Bond";
 const App = () => {
-  return (
-    <div id="child">
-      <h2>Welcome to Adv Full-Stack Programming</h2>
+    return (
+        <Employee firstName={firstName} lastName={lastName} />
+    )
+}
 
-      <Employee firstName={firstName} lastName={lastName} />
-      <Hello />
-    </div>
-  );
-};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const IssueFilter = () => {
+    return (
+        <h2>This is placeholder for IssueFilter</h2>
+    )
+}
 
-root.render(<App />);
+const IssueRow = (props) => {
+    //const rowStyle = { border: "2px solid black" };
+    return (
+        <tr>
+            <td>{props.Id}</td>
+            <td>{props.Status}</td>
+            <td>{props.Owner}</td>
+            <td>{props.Effort}</td>
+            <td>{props.Create.toDateString()}</td>
+            <td>{props.Due.toDateString()}</td>
+            <td>{props.Title}</td>
+
+        </tr>
+    )
+}
+
+const IssueTable = () => {
+
+
+
+
+    //Assume you are fetching data from API
+    const tempIssues = [
+        { Id: 1, Status: "Assigned", Owner: "Person-A", Effort: 14, Create: new Date("2022-09-23"), Due: new Date("2022-09-20"), Title: "This is first isuue" },
+        { Id: 2, Status: "Resolved", Owner: "Person-A", Effort: 10, Create: new Date("2022-09-24"), Due: new Date("2022-09-20"), Title: "This is second isuue" }
+    ];
+
+    const newIssue = 
+        {  Status: "Assigned", Owner: "Person-C", Effort: 24, Create: new Date("2022-09-12"), Due: new Date("2022-09-19"), Title: "This is third isuue" };
+
+    const [allIssues, setAllIssues] = React.useState(tempIssues);
+
+    let counter = 0;
+
+    React.useEffect(() => {
+        console.log(counter);
+        setTimeout(()=>{
+            console.log(counter);
+            counter++;
+            AddSingleIssue(newIssue);
+        },3000)
+    }, [allIssues]);
+
+    /* setTimeout(() => {
+        setAllIssues(tempIssues)
+    }, 2000)
+    */
+
+    const AddSingleIssue = (newIssue) => {
+        console.log(allIssues);
+        newIssue.Id = allIssues.length+1;
+        let issueList = allIssues.slice();
+        
+        issueList.push(newIssue);
+        setAllIssues(issueList);
+        //setAllIssues([...tempIssues,...newIssue])
+    }
+
+    const AllIssuesRow = allIssues.map(issue => (
+        <IssueRow key={issue.Id} Id={issue.Id} Status={issue.Status} Owner={issue.Owner} Effort={issue.Effort} Create={issue.Create} Due={issue.Due} Title={issue.Title}></IssueRow>
+    ))
+
+    return (
+        <div>
+            <h2>This is placeholder for IssueTable</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Effort</th>
+                        <th>Create</th>
+                        <th>Due</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {AllIssuesRow}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+const AddIssue = () => {
+    return (
+        <h2>This is placeholder for IssueFilter</h2>
+    )
+}
+
+const IssueList = () => {
+    return (
+        <div id="issueList">
+            <IssueFilter />
+            <hr />
+            <IssueTable />
+            <hr />
+            <AddIssue />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+        <IssueList />
+)
