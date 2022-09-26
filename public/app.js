0fffcc7dbfa97d2caa433c43c5df785a1d4f6cf2
NishantGautam023 <47427428+NishantGautam023@users.noreply.github.com>
2022-09-25 21:22:30 -0400

    2 files modified

--- public/app.js
+++ public/app.js
@@ -1,38 +1,152 @@
-const IssueTable = () => {
+const IssueRow = props => {
+  // const rowStyle = { "border": "1px solid" }
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, props.Id), /*#__PURE__*/React.createElement("td", null, props.Status), /*#__PURE__*/React.createElement("td", null, props.Owner), /*#__PURE__*/React.createElement("td", null, props.Effort), /*#__PURE__*/React.createElement("td", null, props.Create.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Due.toDateString()), /*#__PURE__*/React.createElement("td", null, props.Title));
+};
+
+const IssueTable = ({
+  allIssues
+}) => {
   const rowStyle = {
-    border: '1px solid black'
-  };
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "IssueTable"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
-    style: rowStyle
-  }, "ID"), /*#__PURE__*/React.createElement("th", {
-    style: rowStyle
-  }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "1"), /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "This is First Issue")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "2"), /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "This is Second Issue")), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "3"), /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, "This is Third Issue")))));
+    border: '1px solid'
+  }; //setting state of variable - hook - rerender the page
+  //Intializing all issues with empty data
+  //const [allIssues, setAllIssues] = React.useState([]);
+  //Assume you are fetching data from API
+  // let tempIssues = [
+  //     {Id:1, Status:"Assigned", Owner:"Person-A", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the First Issue"},
+  //     {Id:2, Status:"Resolves", Owner:"Person-B", Effort:10, Create: new Date("2022-09-19"), Due: new Date("2022-09-24"), Title:"This is the Second Issue"},
+  // ];
+  //lec2
+  // let newIssues = [
+  //     {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"},
+  // ];
+  // let newIssues = {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"};
+  // const [allIssues, setAllIssues] = React.useState(tempIssues);
+  //Hook used after every rendering
+  //use effect (function,dependency) -> called after every rendering
+  // React.useEffect(() => {
+  //     setTimeout(() => {
+  //         //lec2
+  //        // AddSingleIssue();
+  //         AddSingleIssue(newIssues);
+  //    }, 2000)
+  // }, []);
+  //lec2
+  // const AddSingleIssue = () => {
+  //     setAllIssues([...tempIssues, ...newIssues])
+  // }
+  // setTimeout(() => {
+  //      setAllIssues(tempIssues);
+  // }, 2000)
+
+  const AllIssueRow = allIssues.map(issue => /*#__PURE__*/React.createElement(IssueRow, {
+    key: issue.Id,
+    Id: issue.Id,
+    Title: issue.Title,
+    Status: issue.Status,
+    Owner: issue.Owner,
+    Effort: issue.Effort,
+    Create: issue.Create,
+    Due: issue.Due
+  }));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "IssueTable"), /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Id"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Create"), /*#__PURE__*/React.createElement("th", null, "Due"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, AllIssueRow)));
 };
 
 const IssueFilter = () => {
   return /*#__PURE__*/React.createElement("h2", null, "IssueFilter");
 };
 
-const AddIssue = () => {
-  return /*#__PURE__*/React.createElement("h2", null, "AddIssue");
+const AddIssue = ({
+  AddSingleIssue
+}) => {
+  // let newIssues = {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"};
+  //     React.useEffect(() => {
+  //     setTimeout(() => {
+  //         AddSingleIssue(newIssues);
+  //    }, 2000)
+  // }, []);
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = document.forms.addIssue;
+    console.log('fro', form);
+    let newIssues = {
+      Status: form.Status.value,
+      Owner: form.Owner.value,
+      Effort: form.Effort.value,
+      Create: new Date(form.Create.value),
+      Due: new Date(form.Due.value),
+      Title: form.Title.value
+    };
+    AddSingleIssue(newIssues);
+    document.forms.addIssue.reset();
+  };
+
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "AddIssue"), /*#__PURE__*/React.createElement("form", {
+    name: "addIssue",
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Status",
+    placeholder: "Status"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Owner",
+    placeholder: "Owner"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Effort",
+    placeholder: "Effort"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Create",
+    placeholder: "Create"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Due",
+    placeholder: "Due"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "Title",
+    placeholder: "Title"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Submit")));
 };
 
 const IssueList = () => {
+  let temIssues = [{
+    Id: 1,
+    Status: 'Assigned',
+    Owner: 'Person-A',
+    Effort: 14,
+    Create: new Date('2022-09-20'),
+    Due: new Date('2022-09-23'),
+    Title: 'This is the First Issue'
+  }, {
+    Id: 2,
+    Status: 'Resolves',
+    Owner: 'Person-B',
+    Effort: 10,
+    Create: new Date('2022-09-19'),
+    Due: new Date('2022-09-24'),
+    Title: 'This is the Second Issue'
+  }];
+  const [allIssues, setAllIssues] = React.useState(temIssues);
+
+  const AddSingleIssue = newIssues => {
+    newIssues.Id = allIssues.length + 1;
+    let issueList = allIssues.slice();
+    issueList.push(newIssues);
+    setAllIssues(issueList);
+  };
+
   return /*#__PURE__*/React.createElement("div", {
     id: "child"
-  }, /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(AddIssue, null));
+  }, /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, {
+    allIssues: allIssues
+  }), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(AddIssue, {
+    AddSingleIssue: AddSingleIssue
+  }));
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,86 @@
-const IssueTable = () => {
-  const rowStyle = { border: '1px solid black' };
+const IssueRow = (props) => {
+  // const rowStyle = { "border": "1px solid" }
+  return (
+    <tr>
+      <td>{props.Id}</td>
+      <td>{props.Status}</td>
+      <td>{props.Owner}</td>
+      <td>{props.Effort}</td>
+      <td>{props.Create.toDateString()}</td>
+      <td>{props.Due.toDateString()}</td>
+      <td>{props.Title}</td>
+    </tr>
+  );
+};
+
+const IssueTable = ({ allIssues }) => {
+  const rowStyle = { border: '1px solid' };
+  //setting state of variable - hook - rerender the page
+  //Intializing all issues with empty data
+  //const [allIssues, setAllIssues] = React.useState([]);
+
+  //Assume you are fetching data from API
+  // let tempIssues = [
+  //     {Id:1, Status:"Assigned", Owner:"Person-A", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the First Issue"},
+  //     {Id:2, Status:"Resolves", Owner:"Person-B", Effort:10, Create: new Date("2022-09-19"), Due: new Date("2022-09-24"), Title:"This is the Second Issue"},
+  // ];
+
+  //lec2
+  // let newIssues = [
+  //     {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"},
+  // ];
+
+  // let newIssues = {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"};
+  // const [allIssues, setAllIssues] = React.useState(tempIssues);
+
+  //Hook used after every rendering
+  //use effect (function,dependency) -> called after every rendering
+  // React.useEffect(() => {
+  //     setTimeout(() => {
+  //         //lec2
+  //        // AddSingleIssue();
+  //         AddSingleIssue(newIssues);
+  //    }, 2000)
+  // }, []);
+
+  //lec2
+  // const AddSingleIssue = () => {
+  //     setAllIssues([...tempIssues, ...newIssues])
+  // }
+
+  // setTimeout(() => {
+  //      setAllIssues(tempIssues);
+  // }, 2000)
+
+  const AllIssueRow = allIssues.map((issue) => (
+    <IssueRow
+      key={issue.Id}
+      Id={issue.Id}
+      Title={issue.Title}
+      Status={issue.Status}
+      Owner={issue.Owner}
+      Effort={issue.Effort}
+      Create={issue.Create}
+      Due={issue.Due}
+    />
+  ));
+
   return (
     <div>
       <h2>IssueTable</h2>
       <table>
         <thead>
           <tr>
-            <th style={rowStyle}>ID</th>
-            <th style={rowStyle}>Title</th>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Effort</th>
+            <th>Create</th>
+            <th>Due</th>
+            <th>Title</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td style={rowStyle}>1</td>
-            <td style={rowStyle}>This is First Issue</td>
-          </tr>
-          <tr>
-            <td style={rowStyle}>2</td>
-            <td style={rowStyle}>This is Second Issue</td>
-          </tr>
-
-          <tr>
-            <td style={rowStyle}>3</td>
-            <td style={rowStyle}>This is Third Issue</td>
-          </tr>
-        </tbody>
+        <tbody>{AllIssueRow}</tbody>
       </table>
     </div>
   );
@@ -34,17 +90,82 @@ const IssueFilter = () => {
   return <h2>IssueFilter</h2>;
 };
 
-const AddIssue = () => {
-  return <h2>AddIssue</h2>;
+const AddIssue = ({ AddSingleIssue }) => {
+  // let newIssues = {Status:"Assigned", Owner:"Person-C", Effort:14, Create: new Date("2022-09-20"), Due: new Date("2022-09-23"), Title:"This is the Third Issue"};
+  //     React.useEffect(() => {
+  //     setTimeout(() => {
+  //         AddSingleIssue(newIssues);
+  //    }, 2000)
+  // }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = document.forms.addIssue;
+    console.log('fro', form);
+    let newIssues = {
+      Status: form.Status.value,
+      Owner: form.Owner.value,
+      Effort: form.Effort.value,
+      Create: new Date(form.Create.value),
+      Due: new Date(form.Due.value),
+      Title: form.Title.value,
+    };
+    AddSingleIssue(newIssues);
+    document.forms.addIssue.reset();
+  };
+
+  return (
+    <div>
+      <h2>AddIssue</h2>
+      <form name="addIssue" onSubmit={handleSubmit}>
+        <input type="text" name="Status" placeholder="Status" />
+        <input type="text" name="Owner" placeholder="Owner" />
+        <input type="text" name="Effort" placeholder="Effort" />
+        <input type="text" name="Create" placeholder="Create" />
+        <input type="text" name="Due" placeholder="Due" />
+        <input type="text" name="Title" placeholder="Title" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 const IssueList = () => {
+  let temIssues = [
+    {
+      Id: 1,
+      Status: 'Assigned',
+      Owner: 'Person-A',
+      Effort: 14,
+      Create: new Date('2022-09-20'),
+      Due: new Date('2022-09-23'),
+      Title: 'This is the First Issue',
+    },
+    {
+      Id: 2,
+      Status: 'Resolves',
+      Owner: 'Person-B',
+      Effort: 10,
+      Create: new Date('2022-09-19'),
+      Due: new Date('2022-09-24'),
+      Title: 'This is the Second Issue',
+    },
+  ];
+  const [allIssues, setAllIssues] = React.useState(temIssues);
+
+  const AddSingleIssue = (newIssues) => {
+    newIssues.Id = allIssues.length + 1;
+    let issueList = allIssues.slice();
+    issueList.push(newIssues);
+    setAllIssues(issueList);
+  };
+
   return (
     <div id="child">
-      <IssueTable />
-      <hr />
       <IssueFilter />
       <hr />
-      <AddIssue />
+      {/* <IssueTable/> */}
+      <IssueTable allIssues={allIssues} />
+      <hr />
+      <AddIssue AddSingleIssue={AddSingleIssue} />
     </div>
   );
 };
